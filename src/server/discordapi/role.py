f39f242abf49a5f84536977b4a575d4c195a94b8
Gene Ni <geneisjuan@gmail.com>
2023-06-13 23:38:30 -0400

    introduced slash command groupings.
    Will be grouping commands together in the future for a more organized command structure.

--- src/server/discordapi/role.py
+++ src/server/discordapi/role.py
@@ -1,11 +1,14 @@
+from typing import Any, Dict, List, Optional, Union
 import discord
 from discord import app_commands
-from discord.emoji import Emoji
+from discord.app_commands.commands import Group
+from discord.app_commands.translator import locale_str
 from discord.enums import ButtonStyle
 from discord.ext import commands
 from discord.interactions import Interaction
-from discord.partial_emoji import PartialEmoji
+from discord.permissions import Permissions
 from discord.ui import Button, View
+from discord.utils import MISSING
 
 from roleEnum import GAMINGROLE, COLORS
 from roleDbConnector import RoleDatabase
@@ -34,26 +37,15 @@ class RoleButton(discord.ui.Button):
             await interaction.response.send_message(f"{self.role.name} role added.", ephemeral=True)
     
 
-class Roles(commands.Cog):
+@app_commands.default_permissions(administrator=True)
+class Roles(commands.GroupCog):
     
-    
-    def __init__(self) -> None:
+    def __init__(self, bot: commands.Bot) -> None:
         super().__init__()
-        self.gameRoles = {"COC":None, "LOL":None, "CS:GO":None, "OW":None}
+        # self.app_command.default_permissions = discord.Permissions.administrator
+        gameRoles = {"COC":None, "LOL":None, "CS:GO":None, "OW":None}
         
-    
-    def is_guild_admin(self):
-        """
-            Determines if the current ctx is the guild owner and/or bot owner.
-        """
-
-        def predicate(ctx):
-            return ctx.guild is not None and ctx.guild.owner_id == ctx.author.id
-
-        return commands.check(predicate)
-        
-        
-    @app_commands.command(name="buttontest", description="Button Embed Testing")
+    @app_commands.command(name="choose", description="Button Embed Testing")
     async def buttonTest(self, interaction: discord.Interaction):
         """
         Button testing command. Sys bot displays one button. Upon user clicking on the button, the bot responds with a message.
@@ -62,8 +54,6 @@ class Roles(commands.Cog):
             interaction (discord.Interaction): Discord interaction object. Any "interaction" with commands are discord interactions.
         """
 
-        
-        
         # Creates a Discord View object.
         testView = View()
         
@@ -80,11 +70,13 @@ class Roles(commands.Cog):
         await interaction.response.send_message(view=testView)
     
     
-    @app_commands.command(name="extractroles", description="Programming purposes.")
+    
+    
+    @app_commands.command(name="backup_role_database", description="Programming purposes.")
     @commands.check_any(commands.is_owner())
-    async def extractroles(self, interaction: discord.Interaction):
+    async def extractroles(self, interaction: discord.Interaction, backup_db_name: str):
         
-        roleDB = RoleDatabase("role_port")
+        roleDB = RoleDatabase(backup_db_name)
         
         a = interaction.guild.members
         
@@ -98,27 +90,39 @@ class Roles(commands.Cog):
     
     # color.name for color in COLORS
     # https://discordpy.readthedocs.io/en/stable/api.html?highlight=create_role#discord.Guild.create_role
-    @app_commands.command(name="createrole", description="Creates a role based on given parameters.")
+    @app_commands.command(name="create", description="Creates a role based on given parameters.")
     @app_commands.describe(colors="Color selector")
     @app_commands.choices(colors=[discord.app_commands.Choice(name=color.name, value=color.value) for color in COLORS])
-    @commands.check_any(commands.is_owner())
+    
+    # Default permissions mean only admins can access these commands.
+    @app_commands.default_permissions(manage_roles=True, manage_permissions=True)
+    
+    # @app_commands.default_permissions(ban_members=True) Add the permission names and bool as  values to include access.
     async def createrole(self, interaction: discord.Interaction, name: str, colors: discord.app_commands.Choice[int], reason:str):
         
         await interaction.guild.create_role(name=name, colour=discord.Colour(colors.value), reason=reason)
         await interaction.response.send_message(f"Created new role: {name}")
         
     
-    # @app_commands.command(name="removerole", description="Removes a role.")
-    # @app_commands
     
-    @app_commands.command(name="checkpermissions", description="Checks your social status.")
-    @commands.has_permissions(is_guild_admin())
-    async def checkSocialStatus(self, interaction: discord.Interaction):
+    @app_commands.command(name="remove", description="Removes a Role.")
+    @app_commands.default_permissions(manage_roles=True, manage_permissions=True)
+    async def checkSocialStatus(self, interaction: discord.Interaction, role: discord.Role, reason: str = None):
         
-        await interaction.response.send_message("You are indeed an admin!")
+        await role.delete(reason=reason)
+        await interaction.response.send_message(f"Removed role {role.name}")
         
+    
+    @app_commands.command(name="testcommand", description="Test")
+    @app_commands.default_permissions(administrator=True)
+    async def test(self, interaction: discord.Interaction):
         
-async def setup(bot):
-
-  await bot.add_cog(Roles())
+        # print(discord.Permissions._has_flag(discord.Permissions.manage_permissions))
+        
+        await interaction.response.send_message("You can ban members!")
+    
+    
+        
+async def setup(bot: commands.Bot):
+    await bot.add_cog(Roles(bot))
     
